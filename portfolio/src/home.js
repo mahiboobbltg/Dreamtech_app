b5d67bc92267ff8041258cd2f3f4170b70b4ad2f
mahiboob k <mahiboob@b2winfotech.com>
2023-12-08 22:35:02 +0530

    first commit

---
+++ portfolio/src/home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+        
+        
+        <div class="container-fluid mx-auto px-4   bg-gray-500">
+        <h1 className="text-4xl font-bold  text-center">
+      welcome to maahi protfolio
+    </h1>
+</div>
+      
+        
+        
+
+        
+        
+        
+        </div>
+  )
+}
+
+export default Homepage
---
+++ portfolio/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Homepage from './home';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Homepage />
+)
